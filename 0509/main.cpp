b1b192802aff37e634896cc20b854474b8433db2
Unknown <csanalosibalazs@gmail.com>
2017-05-15 05:23:17 +0200

    Kezd a végső állapotba jutni

--- 0509/main.cpp
+++ 0509/main.cpp
@@ -17,14 +17,7 @@
 using namespace std;
 using namespace genv;
 
-
-///////////////////////////////////////////////////
 class ValamiButton;
-
-bool joe(int x)
-{
-    return x<100000000 && x>-100000000;
-}
 
 string nmbrtstrng(int x)
 {
@@ -40,17 +33,6 @@ int strtnbr(string s)
     else return atoi(s.c_str());
 }
 
-/*void addANum(int &O, int x)
-{
-    if (joe(O*10))
-    {
-        O=O*10+x;
-    }
-    //nmbrtstrng(O);
-    //std::cout <<nmbrtstrng(O)<< " d" <<std::endl;
-}*/
-
-
 class JatekMester
 {
     public:
@@ -59,9 +41,7 @@ class JatekMester
 
     void beallit(int i, int j, int v)
     {
-        //cout<<i<<" "<<j<<" "<<v;
         szamok[i][j]=v;
-        //cout<<v;
     }
     bool chekkoljaHogyJoE(int x, int y)
     {
@@ -87,26 +67,15 @@ class JatekMester
                 if (!(i==x && j==y))
                 {
                     if (szamok[i][j]==szamok[x][y]) return szamok[i][j];
-                    //cout<<x<<y<<"'"<<i<<":"<<j<<endl;
                 }
             }
         }
         return false;
     }
-    /*int value = matrix[1][2];
-    matrix[3][1] = 5;*/
 };
 
 JatekMester::JatekMester()
 {
-    /*for (int i=0; i<9; i++)
-        {
-            for (int j=0; j<9; j++)
-            {
-                szamok[i][j]=0;
-                cout<<szamok[i][j];
-            }
-        }*/
 }
 
 
@@ -147,28 +116,6 @@ public:
     OneNumEditor * ed;
     FunctorButton * funbtn1;
     FunctorButton * funbtn2;
-    /*PushButton * button;
-    ValamiButton * vButton;
-    FvPtrButton * fvptrbutton;
-    FunctorButton * functorbutton;
-    FunctorButton * funbtnegyenlo;
-
-    FunctorButton * funbtn2;
-    FunctorButton * funbtn3;
-    FunctorButton * funbtn4;
-    FunctorButton * funbtn5;
-    FunctorButton * funbtn6;
-    FunctorButton * funbtn7;
-    FunctorButton * funbtn8;
-    FunctorButton * funbtn9;
-    FunctorButton * funbtn0;
-    FunctorButton * funbtnPlus;
-    FunctorButton * funbtnMinus;
-    FunctorButton * funbtnDevide;
-    FunctorButton * funbtnMultiply;*/
-    /*int mainValue =0;
-    int currValue =0;
-    char prevOp = '+';*/
     MyWindow() ;
     void esemeny(string mitortent) {
         if (mitortent == "egyenlo") {
@@ -197,7 +144,6 @@ public:
                     mezok[k]->setvalue("");
                 }
             }
-            //cout<<endl;
         }
     }
     void teddBeJocihoz()
@@ -211,9 +157,8 @@ public:
                 bool kalap = Joci->chekkoljaHogyJoE(j,i)<<strtnbr( mezok[k]->value());
                 mezok[k]->setbad(kalap);
             }
-            //cout<<endl;
         }
-        for (int j=0; j<9; j++)//ez azért kell ide mert ettõl jól mûködik
+        for (int j=0; j<9; j++)//ez azért kell ide mert ettõl jól mûködik, ha ezt kiszedjük
         {
             for (int i=0; i<9; i++)
             {
@@ -222,74 +167,11 @@ public:
                 bool kalap = Joci->chekkoljaHogyJoE(j,i)<<strtnbr( mezok[k]->value());
                 mezok[k]->setbad(kalap);
             }
-            //cout<<endl;
         }
     }
-    /*void execute()
-    {
-        if (prevOp=='+')
-        {
-            mainValue+=currValue;
-            currValue=0;
-        }
-        if (prevOp=='-')
-        {
-            mainValue-=currValue;
-            currValue=0;
-        }
-        if (prevOp=='/')
-        {
-            mainValue/=currValue;
-            currValue=0;
-        }
-        if (prevOp=='*')
-        {
-            mainValue*=currValue;
-            currValue=0;
-        }
-        if (prevOp=='=')
-        {
-            //currValue=mainValue;
-        }
-        tx1->setText(nmbrtstrng(currValue));
-    }*/
     void valami() {
         tx1->setText(ed1->value());
     }
-    /*
-    void egyenlo() {
-        execute();
-        prevOp='=';
-        tx1->setText(nmbrtstrng(mainValue));
-    }
-    void btnN(int x) {
-        if (joe(currValue*10))
-        {
-            currValue=currValue*10+x;
-        }
-        std::cout <<currValue<<std::endl;
-        tx1->setText(nmbrtstrng(currValue));
-    }
-    void plusz()
-    {
-        execute();
-        prevOp='+';
-    }
-    void minusz()
-    {
-        execute();
-        prevOp='-';
-    }
-    void devide()
-    {
-        execute();
-        prevOp='/';
-    }
-    void multiply()
-    {
-        execute();
-        prevOp='*';
-    }*/
 
 };
 
@@ -318,54 +200,10 @@ MyWindow::MyWindow() {
                 //std::cout<<ed->getKorx()<<endl;
             }
         }
-        for (int i=0; i<81; i++)
-        {
-                //std::cout<<i<<" "<<mezok[i]->getKorx()<<endl;
-        }
-        /*//button = new MessageButton(this, 10, 10, 60, 60,"oda", "egyenlo");
-        //tx1 = new StaticText(75,20,100,40,"0");
-        //ed1 = new TextEditor(10,100,100,40,"");
-        //ed = new OneNumEditor(10,100,25,25,"",false);
-        //vButton = new ValamiButton(this, 10,200,100,40,"vbut");
-        //fvptrbutton = new FvPtrButton(this, 10,250,100,40,"fvptr",MyWindow::fv);
-        //functorbutton = new FunctorButton(10,300,100,40,"functor",[this](){this->valami();});
-        /*funbtnegyenlo = new FunctorButton(240,320,80,40,"=",[this](){this->egyenlo();});*/
         funbtn1 = new FunctorButton(30,20,100,40,"ellenorzes",[this](){this->teddBeJocihoz();});
         funbtn2 = new FunctorButton(200,20,100,40,"uj palya",[this](){this->palyavaltas();});
-        /*funbtn3 = new FunctorButton(280,200,40,40,"3",[this](){this->btnN(3);});
-        funbtn4 = new FunctorButton(200,240,40,40,"4",[this](){this->btnN(4);});
-        funbtn5 = new FunctorButton(240,240,40,40,"5",[this](){this->btnN(5);});
-        funbtn6 = new FunctorButton(280,240,40,40,"6",[this](){this->btnN(6);});
-        funbtn7 = new FunctorButton(200,280,40,40,"7",[this](){this->btnN(7);});
-        funbtn8 = new FunctorButton(240,280,40,40,"8",[this](){this->btnN(8);});
-        funbtn9 = new FunctorButton(280,280,40,40,"9",[this](){this->btnN(9);});
-        funbtn0 = new FunctorButton(200,320,40,40,"0",[this](){this->btnN(0);});
-        funbtnPlus = new FunctorButton(320,320,40,40,"+",[this](){this->plusz();});
-        funbtnMinus = new FunctorButton(320,280,40,40,"-",[this](){this->minusz();});
-        funbtnDevide = new FunctorButton(320,200,40,40,"/",[this](){this->devide();});
-        funbtnMultiply = new FunctorButton(320,240,40,40,"*",[this](){this->multiply();});
-        //widgets.push_back(tx1);
-        //widgets.push_back(ed1);
-        //widgets.push_back(ed);
-        //widgets.push_back(button);
-        //widgets.push_back(vButton);
-        //widgets.push_back(fvptrbutton);
-        //widgets.push_back(functorbutton );
-        /*widgets.push_back(funbtnegyenlo);*/
         widgets.push_back(funbtn1);
         widgets.push_back(funbtn2);
-        /*widgets.push_back(funbtn3);
-        widgets.push_back(funbtn4);
-        widgets.push_back(funbtn5);
-        widgets.push_back(funbtn6);
-        widgets.push_back(funbtn7);
-        widgets.push_back(funbtn8);
-        widgets.push_back(funbtn9);
-        widgets.push_back(funbtn0);
-        widgets.push_back(funbtnPlus);
-        widgets.push_back(funbtnMinus);
-        widgets.push_back(funbtnDevide);
-        widgets.push_back(funbtnMultiply);*/
 }
 
 
